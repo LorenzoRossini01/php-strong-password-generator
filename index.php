e0a046b666b0aa4d90df6b1a7d11e8ea81e23b74
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-02-28 16:57:02 +0100

    feat: add filter char password gen

--- index.php
+++ index.php
@@ -8,13 +8,22 @@ $numbers=explode(' ','1 2 3 4 5 6 7 8 9 0');
 $letters=explode(' ','a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z');
 $simbols=explode(' ','! " £ € $ % & / ( ) = ? * _ - . : , ; ç @ # ] [ { }');
 
-// unire i 3 array in uno solo 
-$all_char= array_merge($numbers,$letters,$simbols);
-$password=[];
+$all_char=[];
 
 $form_sent=!empty($_GET);
 $digit_number=isset($_GET['digit-number'])?(int)$_GET['digit-number']:'';
+$select_numbers=isset($_GET['number'])?$_GET['number']:'';
+$select_letters=isset($_GET['letters'])?$_GET['letters']:'';
+$select_simbols=isset($_GET['simbols'])?$_GET['simbols']:'';
 
+
+if($select_numbers=='on') $all_char= array_merge($all_char,$numbers);
+if($select_letters=='on') $all_char= array_merge($all_char,$letters);
+if($select_simbols=='on') $all_char= array_merge($all_char,$simbols);
+
+// unire i 3 array in uno solo 
+// $all_char= array_merge($numbers,$letters,$simbols);
+$password=[];
 
 
 if($form_sent){
@@ -27,9 +36,9 @@ if($form_sent){
     
     session_start();
     $_SESSION['password']=$password_str;
-    header('Location: ./newpsw.php');
+    // header('Location: ./newpsw.php');
 
-
+var_dump($password_str);
 
 
 }
@@ -55,9 +64,25 @@ if($form_sent){
         <div class="card-header">Crea la tua password</div>
         <div class="card-body">
             <form method="GET" class="row g-3">
-                <div class="col">
+                <div class="col-12">
                     <label for="digit-number" class="form-label">Di quanti caratteri sarà composta la tua password</label>
-                    <input type="number" min="4" max="20" name="digit-number" id="digit-number" class="form-control" value=<?= $form_sent? $digit_number:''?>>
+                    <input type="number" min="4"  name="digit-number" id="digit-number" class="form-control" value=<?= $form_sent? $digit_number:''?>>
+                </div>
+                <div class="col-2">
+                        <div class="form-check">
+                            <label for="number" class="form-check-label">Numeri</label>
+                            <input type="checkbox" name="number" id="number" class="form-check-input" <?= $select_numbers? 'checked':''?>>
+                        </div>
+
+                        <div class="form-check">
+                            <label for="letters" class="form-check-label">Lettere</label>
+                            <input type="checkbox" name="letters" id="letters" class="form-check-input" <?= $select_letters? 'checked':''?>>
+                        </div>
+
+                        <div class="form-check">
+                            <label for="simbols" class="form-check-label">Simboli</label>
+                            <input type="checkbox" name="simbols" id="simbols" class="form-check-input" <?= $select_simbols? 'checked':''?>>
+                        </div>
                 </div>
                 <div class="col-12">
                     <button class="btn btn-primary">Genera</button>
